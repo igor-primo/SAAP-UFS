db0aff0a07f5a8f21faef4c5c0bdb1b979d58473
igor-primo <igorprimo62@gmail.com>
2022-04-27 15:23:08 -0300

    get formulario

--- models/formulario.js
+++ models/formulario.js
@@ -78,8 +78,50 @@ async function post_formularios_cadastrados(
 
 }
 
+async function get_formularios_cadastrados(id_proj){
+	try {
+		const query_id_form = db.query(
+			`SELECT f.id FROM
+				formulario AS f
+			INNER JOIN
+				projeto AS p
+			ON f.fk_proj = p.id
+			WHERE p.id = $1;`,
+			[ id_proj ]
+		);
+		const id_form = query_id_form.rows[0]id;
+		const query_secao_quest = db.query(
+			`SELECT sq.id, sq.nome_sec FROM
+				secao_quest AS sq
+			INNER JOIN formulario AS f
+			ON sq.fk_form = f.id
+			WHERE f.id = $1;`,
+			[ id_form ]
+		);
+		let form;
+		let form_i = 0;
+		const secoes = query_secao_quest.rows;
+		for(let i=0;i<secoes.length;i++){
+			const query_quesito = db.query(
+				`SELECT q.pergunta FROM
+					quesito AS q
+				INNER JOIN secao_quest AS sq
+				ON q.fk_secao = sq.id
+				WHERE sq.id = $1;`,
+				[ secoes[i].id ]
+			);
+			form[form_i++] = secoes[i].nome_sec;
+			form[form_i] = query_quesito.rows;
+		}
+		return form;
+	} catch(e){
+		throw e;
+	}
+}
+
 module.exports = {
 
-	post_formularios_cadastrados
+	post_formularios_cadastrados,
+	get_formularios_cadastrados
 
 };
