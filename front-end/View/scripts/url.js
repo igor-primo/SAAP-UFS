2235a16dc3b188bc3610fa79f30780c549d91d08
igor-primo <igorprimo62@gmail.com>
2022-05-15 13:48:07 -0300

    url

--- front-end/View/scripts/url.js
+++ front-end/View/scripts/url.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://127.0.0.1:5000';
-//const BASE_URL = 'https://saap-ufs.herokuapp.com';
+//const BASE_URL = 'http://127.0.0.1:5000';
+const BASE_URL = 'https://saap-ufs.herokuapp.com';
 
 export default BASE_URL;
